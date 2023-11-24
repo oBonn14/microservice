a7ab87c88601ff065d653bd927003e68b9d14c83
BonBon153 <exsanal1515@gmail.com>
2023-11-24 09:20:38 +0700

    Update All Service

--- consumer-redis/src/main/java/com/talentreadiness/consumerredis/service/RedisService.java
+++ consumer-redis/src/main/java/com/talentreadiness/consumerredis/service/RedisService.java
@@ -6,16 +6,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.talentreadiness.database.dto.CustomerDTO;
 import com.talentreadiness.database.dto.MessageDTO;
-import com.talentreadiness.database.model.Customer;
-import com.talentreadiness.database.repository.CustomerRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,7 +20,6 @@ import lombok.RequiredArgsConstructor;
 public class RedisService {
 
     private final RedisTemplate redisTemplate;
-    private final CustomerRepository customerRepository;
     private static final String KEY = "CUSTOMER";
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisService.class);
 
